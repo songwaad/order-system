b952d32b19a68e669c3e8dac9f4a12568653b82a
Songwaad H <songw44d@gmail.com>
2025-10-15 23:19:53 +0700

    refactor: Rename middleware folder to auth

---
+++ Infra/auth.go
@@ -0,0 +1,42 @@
+package infra
+
+import (
+	"kornkk/entities"
+	"net/mail"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func IsEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func JwtClaims(user *entities.User) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = user.ID
+	claims["username"] = user.Username
+	claims["email"] = user.Email
+	claims["role"] = user.UserRole.Name
+
+	t, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return t, nil
+}
